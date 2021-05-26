073b8de2e659ca79b0827e01e3763b15767320bd
verozgames <yoan.laurain0@gmail.com>
2021-05-26 13:55:10 +0200

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmMotus
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMotus));
+            this.labelTailleMot = new System.Windows.Forms.Label();
+            this.buttonTailleMotOuvrir = new System.Windows.Forms.Button();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.textBoxChoix = new System.Windows.Forms.TextBox();
+            this.buttonValider = new System.Windows.Forms.Button();
+            this.dataGridViewMotus = new System.Windows.Forms.DataGridView();
+            this.buttonJouer = new System.Windows.Forms.Button();
+            this.buttonAnnuler = new System.Windows.Forms.Button();
+            this.buttonQuitter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMotus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTailleMot
+            // 
+            this.labelTailleMot.AutoSize = true;
+            this.labelTailleMot.Location = new System.Drawing.Point(234, 30);
+            this.labelTailleMot.Name = "labelTailleMot";
+            this.labelTailleMot.Size = new System.Drawing.Size(126, 13);
+            this.labelTailleMot.TabIndex = 0;
+            this.labelTailleMot.Text = "Choisir une liste de mots :";
+            // 
+            // buttonTailleMotOuvrir
+            // 
+            this.buttonTailleMotOuvrir.Location = new System.Drawing.Point(572, 25);
+            this.buttonTailleMotOuvrir.Name = "buttonTailleMotOuvrir";
+            this.buttonTailleMotOuvrir.Size = new System.Drawing.Size(75, 23);
+            this.buttonTailleMotOuvrir.TabIndex = 1;
+            this.buttonTailleMotOuvrir.Text = "Ouvrir";
+            this.buttonTailleMotOuvrir.UseVisualStyleBackColor = true;
+            this.buttonTailleMotOuvrir.Click += new System.EventHandler(this.buttonTailleMotOuvrir_Click);
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(366, 27);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(200, 20);
+            this.textBoxFile.TabIndex = 2;
+            // 
+            // textBoxChoix
+            // 
+            this.textBoxChoix.Location = new System.Drawing.Point(301, 71);
+            this.textBoxChoix.Name = "textBoxChoix";
+            this.textBoxChoix.Size = new System.Drawing.Size(200, 20);
+            this.textBoxChoix.TabIndex = 3;
+            this.textBoxChoix.TextChanged += new System.EventHandler(this.textBoxChoix_TextChanged);
+            this.textBoxChoix.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxChoix_KeyDown);
+            // 
+            // buttonValider
+            // 
+            this.buttonValider.Location = new System.Drawing.Point(507, 69);
+            this.buttonValider.Name = "buttonValider";
+            this.buttonValider.Size = new System.Drawing.Size(75, 23);
+            this.buttonValider.TabIndex = 4;
+            this.buttonValider.Text = "Valider";
+            this.buttonValider.UseVisualStyleBackColor = true;
+            this.buttonValider.Click += new System.EventHandler(this.buttonValider_Click);
+            // 
+            // dataGridViewMotus
+            // 
+            this.dataGridViewMotus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMotus.Location = new System.Drawing.Point(188, 111);
+            this.dataGridViewMotus.Name = "dataGridViewMotus";
+            this.dataGridViewMotus.Size = new System.Drawing.Size(500, 280);
+            this.dataGridViewMotus.TabIndex = 5;
+            // 
+            // buttonJouer
+            // 
+            this.buttonJouer.Location = new System.Drawing.Point(201, 423);
+            this.buttonJouer.Name = "buttonJouer";
+            this.buttonJouer.Size = new System.Drawing.Size(150, 50);
+            this.buttonJouer.TabIndex = 7;
+            this.buttonJouer.Text = "Jouer";
+            this.buttonJouer.UseVisualStyleBackColor = true;
+            this.buttonJouer.Click += new System.EventHandler(this.buttonJouer_Click);
+            // 
+            // buttonAnnuler
+            // 
+            this.buttonAnnuler.Location = new System.Drawing.Point(357, 423);
+            this.buttonAnnuler.Name = "buttonAnnuler";
+            this.buttonAnnuler.Size = new System.Drawing.Size(150, 50);
+            this.buttonAnnuler.TabIndex = 8;
+            this.buttonAnnuler.Text = "Annuler";
+            this.buttonAnnuler.UseVisualStyleBackColor = true;
+            this.buttonAnnuler.Click += new System.EventHandler(this.buttonAnnuler_Click);
+            // 
+            // buttonQuitter
+            // 
+            this.buttonQuitter.Location = new System.Drawing.Point(513, 423);
+            this.buttonQuitter.Name = "buttonQuitter";
+            this.buttonQuitter.Size = new System.Drawing.Size(150, 50);
+            this.buttonQuitter.TabIndex = 9;
+            this.buttonQuitter.Text = "Quitter";
+            this.buttonQuitter.UseVisualStyleBackColor = true;
+            this.buttonQuitter.Click += new System.EventHandler(this.buttonQuitter_Click);
+            // 
+            // frmMotus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(864, 502);
+            this.Controls.Add(this.buttonQuitter);
+            this.Controls.Add(this.buttonAnnuler);
+            this.Controls.Add(this.buttonJouer);
+            this.Controls.Add(this.dataGridViewMotus);
+            this.Controls.Add(this.buttonValider);
+            this.Controls.Add(this.textBoxChoix);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.buttonTailleMotOuvrir);
+            this.Controls.Add(this.labelTailleMot);
+            this.MaximumSize = new System.Drawing.Size(880, 540);
+            this.MinimumSize = new System.Drawing.Size(880, 540);
+            this.Name = "frmMotus";
+            this.Text = "Motus";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMotus)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTailleMot;
+        private System.Windows.Forms.Button buttonTailleMotOuvrir;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.TextBox textBoxChoix;
+        private System.Windows.Forms.Button buttonValider;
+        private System.Windows.Forms.DataGridView dataGridViewMotus;
+        private System.Windows.Forms.Button buttonJouer;
+        private System.Windows.Forms.Button buttonAnnuler;
+        private System.Windows.Forms.Button buttonQuitter;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class frmMotus : Form
+    {
+        // Defini la reponse
+        string reponse;                
+        int nbessai, nbessaimax = 7;
+
+        //==============================================================================
+
+        public frmMotus()
+        {
+            InitializeComponent();
+        }
+
+        //==============================================================================
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Fond des controls transparent
+            labelTailleMot.BackColor = Color.Transparent;
+            labelTailleMot.ForeColor = Color.White;
+
+            // Desactive les éléments
+            buttonValider.Enabled = false;
+            textBoxChoix.Enabled = false;
+            buttonAnnuler.Enabled = false;
+            dataGridViewMotus.Visible = false;
+
+            // Defini les specs du tableau
+            dataGridViewMotus.ColumnHeadersVisible = false;
+            dataGridViewMotus.Enabled = false;
+            dataGridViewMotus.RowHeadersVisible = false;
+            dataGridViewMotus.AllowUserToResizeColumns = false;
+            dataGridViewMotus.AllowUserToResizeRows = false;
+            dataGridViewMotus.ScrollBars = ScrollBars.None;
+            dataGridViewMotus.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+        }
+
+        //==============================================================================
+        
+        private void textBoxChoix_TextChanged(object sender, EventArgs e)
+        {
+            // Converti en majuscule
+            textBoxChoix.CharacterCasing = CharacterCasing.Upper;
+            textBoxChoix.MaxLength = Convert.ToInt16(reponse.Length);
+            // Test la taille
+            if (textBoxChoix.Text.Length == reponse.Length)
+            {
+                buttonValider.Enabled = true;
+            }
+            else
+            {
+                buttonValider.Enabled = false;
+            }
+
+
+        }
+
+        private void textBoxChoix_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Efface les caracteres interdit
+            if (((int)e.KeyCode >= 1 && (int)e.KeyCode <= 7)
+            || ((int)e.KeyCode >= 9 && (int)e.KeyCode <= 36)
+            || ((int)e.KeyCode >= 40 && (int)e.KeyCode <= 45)
+            || ((int)e.KeyCode >= 47 && (int)e.KeyCode <= 64)
+            || ((int)e.KeyCode == 38)
+            || ((int)e.KeyCode > 90)
+            )
+            {
+                e.SuppressKeyPress = true;
+            }
+           
+
+
+        }
+
+        //==============================================================================
+
+        private void buttonTailleMotOuvrir_Click(object sender, EventArgs e)
+        {
+            // Recupere le fichier de mots
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.CheckFileExists = true;
+            fileDialog.CheckPathExists = true;
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFile.Text = fileDialog.FileName;
+            }
+            fileDialog.Dispose();
+        }
+
+        //==============================================================================
+
+        private void buttonJouer_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBoxFile.Text))
+            {
+                MessageBox.Show("Aucune liste de mot n'a été sélectionner !", "Motus", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Efface le tableau
+            dataGridViewMotus.Rows.Clear();
+            dataGridViewMotus.Columns.Clear();
+
+            // Nombre de colonne
+            string[] lines = File.ReadAllLines(textBoxFile.Text);
+            Random random = new Random();
+            reponse = lines[random.Next(0, lines.Count())].ToUpper();
+            dataGridViewMotus.ColumnCount = reponse.Length;
+            dataGridViewMotus.RowCount = 7;
+
+
+            // Affiche le et déplace et resize tableau
+            dataGridViewMotus.Visible = true;
+
+            dataGridViewMotus.Left = 880 / 2 - reponse.Length * 40 / 2 - 8;
+            dataGridViewMotus.Top = 110;
+
+            dataGridViewMotus.Height = 7 * 40;
+            dataGridViewMotus.Width = reponse.Length * 40;
+
+
+            // Change la taille des colonnes
+            for (int i = 0; i < dataGridViewMotus.ColumnCount; i++)
+            {
+                dataGridViewMotus.Columns[i].Width = 40;
+            }
+            // Change la taille des lignes
+            for (int i = 0; i < dataGridViewMotus.RowCount; i++)
+            {
+                dataGridViewMotus.Rows[i].Height = 40;
+            }
+
+            // Debloque les controls
+            textBoxChoix.Enabled = true;
+            buttonAnnuler.Enabled = true;
+            dataGridViewMotus.Visible = true;
+
+            textBoxFile.Enabled = false;
+            buttonTailleMotOuvrir.Enabled = false;
+            buttonJouer.Enabled = false;
+
+            // Efface ça
+            textBoxChoix.Text = "";
+
+            // Affiche le premier caractere
+            dataGridViewMotus.Rows[0].Cells[0].Value = reponse[0];           
+            // Init le nb d'essai
+            nbessai = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                dataGridViewMotus.Rows[i].Cells[0].Value = reponse[0];
+           
+            }
+            textBoxChoix.Text = Convert.ToString(reponse[0]);
+
+            for (int i = 0; i < 7; i++)
+            {
+                dataGridViewMotus.Rows[i].Cells[0].Value = reponse[0];
+            }
+        }
+
+        //==============================================================================
+
+        private void buttonQuitter_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //==============================================================================
+
+        private void buttonAnnuler_Click(object sender, EventArgs e)
+        {
+            // Cache le tableau
+            dataGridViewMotus.Visible = false;
+
+            // Bloque les controls
+            buttonValider.Enabled = false;
+            textBoxChoix.Enabled = false;
+            buttonAnnuler.Enabled = false;
+            dataGridViewMotus.Visible = false;
+
+            textBoxFile.Enabled = true;
+            buttonTailleMotOuvrir.Enabled = true;
+            buttonJouer.Enabled = true;
+        }
+
+        //==============================================================================
+
+        private void buttonValider_Click(object sender, EventArgs e)
+        {
+           
+            String tmp = "";
+            for (int i = 0; i < reponse.Length; i++)
+            {
+                dataGridViewMotus.Rows[nbessai].Cells[i].Value = textBoxChoix.Text[i];
+
+                // Affiche en rouge si bon emplacement
+                if (reponse[i] == textBoxChoix.Text[i])
+                {
+                    dataGridViewMotus.Rows[nbessai].Cells[i].Style.BackColor = Color.Red;
+                    tmp += textBoxChoix.Text[i];
+                }
+                else
+                {
+                    // Verifi que la suite du mot ne contient pas le caractere (sinon le traiter plus tard), et que le caractere ne soit pas deja ajouter trop de fois
+                    if ((!(reponse.Substring(i).Contains(textBoxChoix.Text[i]))) && ((tmp.Split(textBoxChoix.Text[i]).Length - 1) < (reponse.Split(textBoxChoix.Text[i]).Length - 1)))
+                    {
+                        dataGridViewMotus.Rows[nbessai].Cells[i].Style.BackColor = Color.Yellow;
+                        tmp += textBoxChoix.Text[i];
+                    }
+                }
+            }
+           
+            nbessai++;
+            if ((nbessai == nbessaimax) || (reponse == textBoxChoix.Text))
+            {
+                // Affiche le message
+                if (reponse == textBoxChoix.Text)
+                {
+                    MessageBox.Show("Vous avez gagné ! Le mot est bien : " + reponse + ".", "Motus", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    MessageBox.Show("Vous avez perdu... Le mot est : " + reponse + ".", "Motus", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                // Cache le tableau
+                dataGridViewMotus.Visible = false;
+
+                // Bloque les controls
+                buttonValider.Enabled = false;
+                textBoxChoix.Enabled = false;
+                buttonAnnuler.Enabled = false;
+                dataGridViewMotus.Visible = false;
+
+                textBoxFile.Enabled = true;
+                buttonTailleMotOuvrir.Enabled = true;
+                buttonJouer.Enabled = true;
+               
+            }
+           
+            textBoxChoix.Text = "";
+        }
+       
+        //==============================================================================
+    }
+}
