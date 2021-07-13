a51cd3c60e1416eb53b7e2589da613d55a87da51
verozgames <yoan.laurain0@gmail.com>
2021-07-13 18:53:40 +0200

    FIX GAME

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -14,7 +14,8 @@ namespace WindowsFormsApp1
 {
     public partial class frmMotus : Form
     {
-        // Defini la reponse
+        //==============================================================================
+
         string reponse;                
         int nbessai, nbessaimax = 7;
 
@@ -29,18 +30,20 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-            // Fond des controls transparent
             labelTailleMot.BackColor = Color.Transparent;
             labelTailleMot.ForeColor = Color.White;
 
-            // Desactive les éléments
+            textBoxFile.Text = "..\\..\\..\\mots8.txt";
+
+            // Desactive les éléments//
+
             buttonValider.Enabled = false;
             textBoxChoix.Enabled = false;
             buttonAnnuler.Enabled = false;
             dataGridViewMotus.Visible = false;
 
-            // Defini les specs du tableau
+            // Defini les paramètres du tableau//
+
             dataGridViewMotus.ColumnHeadersVisible = false;
             dataGridViewMotus.Enabled = false;
             dataGridViewMotus.RowHeadersVisible = false;
@@ -48,17 +51,19 @@ namespace WindowsFormsApp1
             dataGridViewMotus.AllowUserToResizeRows = false;
             dataGridViewMotus.ScrollBars = ScrollBars.None;
             dataGridViewMotus.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-
         }
 
         //==============================================================================
         
         private void textBoxChoix_TextChanged(object sender, EventArgs e)
         {
-            // Converti en majuscule
+            //Convertion des entrées en majuscules//
+
             textBoxChoix.CharacterCasing = CharacterCasing.Upper;
             textBoxChoix.MaxLength = Convert.ToInt16(reponse.Length);
-            // Test la taille
+
+            //Verification de la taille du mot entrée//
+
             if (textBoxChoix.Text.Length == reponse.Length)
             {
                 buttonValider.Enabled = true;
@@ -67,13 +72,16 @@ namespace WindowsFormsApp1
             {
                 buttonValider.Enabled = false;
             }
-
-
         }
+
+        //==============================================================================
+
+        //==============================================================================
 
         private void textBoxChoix_KeyDown(object sender, KeyEventArgs e)
         {
-            // Efface les caracteres interdit
+            // Efface les caracteres interdit//
+
             if (((int)e.KeyCode >= 1 && (int)e.KeyCode <= 7)
             || ((int)e.KeyCode >= 9 && (int)e.KeyCode <= 36)
             || ((int)e.KeyCode >= 40 && (int)e.KeyCode <= 45)
@@ -84,16 +92,16 @@ namespace WindowsFormsApp1
             {
                 e.SuppressKeyPress = true;
             }
-           
-
-
         }
+
+        //==============================================================================
 
         //==============================================================================
 
         private void buttonTailleMotOuvrir_Click(object sender, EventArgs e)
         {
-            // Recupere le fichier de mots
+            // Recupère le fichier de mots// 
+
             OpenFileDialog fileDialog = new OpenFileDialog();
             fileDialog.CheckFileExists = true;
             fileDialog.CheckPathExists = true;
@@ -107,48 +115,60 @@ namespace WindowsFormsApp1
 
         //==============================================================================
 
+        //==============================================================================
+
         private void buttonJouer_Click(object sender, EventArgs e)
         {
+            //Si le fichier n'existe pas //
+
             if (!File.Exists(textBoxFile.Text))
             {
                 MessageBox.Show("Aucune liste de mot n'a été sélectionner !", "Motus", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
-            // Efface le tableau
+            //Efface le tableau//
+
             dataGridViewMotus.Rows.Clear();
             dataGridViewMotus.Columns.Clear();
 
-            // Nombre de colonne
+            //Récup les lignes du fichier//
+
             string[] lines = File.ReadAllLines(textBoxFile.Text);
+
+            /*Prend un nombre aléatoire compris entre le nombre de lignes du fichier pour récupération d'un 
+             * mot puis mise en majuscule et en fin affectation de la longueur du tableau en proportion de 
+             * la longueur du mot */
+
             Random random = new Random();
             reponse = lines[random.Next(0, lines.Count())].ToUpper();
             dataGridViewMotus.ColumnCount = reponse.Length;
+
+            //Paramètres du tableau//
+
             dataGridViewMotus.RowCount = 7;
-
-
-            // Affiche le et déplace et resize tableau
             dataGridViewMotus.Visible = true;
-
             dataGridViewMotus.Left = 880 / 2 - reponse.Length * 40 / 2 - 8;
             dataGridViewMotus.Top = 110;
-
             dataGridViewMotus.Height = 7 * 40;
             dataGridViewMotus.Width = reponse.Length * 40;
 
+            //Change la taille des colonnes//
 
-            // Change la taille des colonnes
             for (int i = 0; i < dataGridViewMotus.ColumnCount; i++)
             {
                 dataGridViewMotus.Columns[i].Width = 40;
             }
-            // Change la taille des lignes
+
+            //Change la taille des lignes//
+
             for (int i = 0; i < dataGridViewMotus.RowCount; i++)
             {
                 dataGridViewMotus.Rows[i].Height = 40;
             }
 
-            // Debloque les controls
+            //Debloque les controles//
+
             textBoxChoix.Enabled = true;
             buttonAnnuler.Enabled = true;
             dataGridViewMotus.Visible = true;
@@ -157,25 +177,27 @@ namespace WindowsFormsApp1
             buttonTailleMotOuvrir.Enabled = false;
             buttonJouer.Enabled = false;
 
-            // Efface ça
             textBoxChoix.Text = "";
 
-            // Affiche le premier caractere
-            dataGridViewMotus.Rows[0].Cells[0].Value = reponse[0];           
-            // Init le nb d'essai
+            //Affiche le premier caractère//
+
+            dataGridViewMotus.Rows[0].Cells[0].Value = reponse[0];     
+            
             nbessai = 0;
-            for (int i = 0; i < 7; i++)
-            {
-                dataGridViewMotus.Rows[i].Cells[0].Value = reponse[0];
-           
-            }
-            textBoxChoix.Text = Convert.ToString(reponse[0]);
+
+            //Affiche le premier caractère sur toutes les lignes//
 
             for (int i = 0; i < 7; i++)
             {
                 dataGridViewMotus.Rows[i].Cells[0].Value = reponse[0];
             }
+
+            //Ecrit la première lettre dans la proposition//
+
+            textBoxChoix.Text = Convert.ToString(reponse[0]);
         }
+
+        //==============================================================================
 
         //==============================================================================
 
@@ -186,12 +208,11 @@ namespace WindowsFormsApp1
 
         //==============================================================================
 
+        //==============================================================================
+
         private void buttonAnnuler_Click(object sender, EventArgs e)
         {
-            // Cache le tableau
             dataGridViewMotus.Visible = false;
-
-            // Bloque les controls
             buttonValider.Enabled = false;
             textBoxChoix.Enabled = false;
             buttonAnnuler.Enabled = false;
@@ -204,35 +225,105 @@ namespace WindowsFormsApp1
 
         //==============================================================================
 
+        //==============================================================================
+
         private void buttonValider_Click(object sender, EventArgs e)
         {
-           
-            String tmp = "";
+            //Variables contenant toutes les lettres jaunes trouver//
+
+            String LettreJaune = "";
+            String LettreRouge = "";
+
+            //Parcours de la réponse pour les lettres rouges//
+
             for (int i = 0; i < reponse.Length; i++)
             {
+                //Remplie le dgv avec les valeurs du mot essayé//
+
                 dataGridViewMotus.Rows[nbessai].Cells[i].Value = textBoxChoix.Text[i];
 
-                // Affiche en rouge si bon emplacement
+                // Affiche en rouge si bon emplacement//
+
                 if (reponse[i] == textBoxChoix.Text[i])
                 {
                     dataGridViewMotus.Rows[nbessai].Cells[i].Style.BackColor = Color.Red;
-                    tmp += textBoxChoix.Text[i];
+                    LettreRouge += textBoxChoix.Text[i];
+                    LettreJaune += textBoxChoix.Text[i];
+                }       
+            }
+
+            //Parcours de la réponse pour les lettres Jaunes//
+
+            for (int i=1; i<reponse.Length; i++)
+            {
+                //Si la lettre actuel est contenue dans les lettres rouges 
+
+                if(LettreRouge.Contains(textBoxChoix.Text[i]))
+                {
+                    if (NbFoisLettreDansMot(reponse, textBoxChoix.Text[i]) > NbFoisLettreDansMot(LettreJaune, textBoxChoix.Text[i]))
+                    {
+                        if(dataGridViewMotus.Rows[nbessai].Cells[i].Style.BackColor != Color.Red)
+                        {
+                            dataGridViewMotus.Rows[nbessai].Cells[i].Style.BackColor = Color.Yellow;
+                            LettreJaune += textBoxChoix.Text[i];
+                        }
+                    }
                 }
                 else
                 {
-                    // Verifi que la suite du mot ne contient pas le caractere (sinon le traiter plus tard), et que le caractere ne soit pas deja ajouter trop de fois
-                    if ((!(reponse.Substring(i).Contains(textBoxChoix.Text[i]))) && ((tmp.Split(textBoxChoix.Text[i]).Length - 1) < (reponse.Split(textBoxChoix.Text[i]).Length - 1)))
+                    //Si la lettre actuel est dejà contenue dans les lettres jaunes
+
+                    if(LettreJaune.Contains(textBoxChoix.Text[i]))
                     {
-                        dataGridViewMotus.Rows[nbessai].Cells[i].Style.BackColor = Color.Yellow;
-                        tmp += textBoxChoix.Text[i];
+                        //Verfie que il y ait d'autre occurence de la même lettre qui ne soit pas encore jaune
+                        /*
+                         * Exemple : Reponse : Bannanes Mot proposé : Bastilla-> Si le premier A est dejà jaune et que on est sur le 2 ème A
+                         * On verifie que dans la réponse il y est un 2ème A pas déja jaune 
+                         * Ducoup si oui -> jaune Sinon -> Sa veut dire que la lettre en cours est déja jaune autre part mais n'apparait
+                         * une autre fois dans le mot donc on ne l'a met pas en jaune
+                         */
+                        if (NbFoisLettreDansMot(reponse, textBoxChoix.Text[i]) > NbFoisLettreDansMot(LettreJaune, textBoxChoix.Text[i]))
+                        {
+                            dataGridViewMotus.Rows[nbessai].Cells[i].Style.BackColor = Color.Yellow;
+                            LettreJaune += textBoxChoix.Text[i];
+
+                            MessageBox.Show("La lettre : " + textBoxChoix.Text[i] + "est : " + NbFoisLettreDansMot(reponse, textBoxChoix.Text[i]) + " dans la réponse ");
+                            MessageBox.Show("La lettre : " + textBoxChoix.Text[i] + "est : " + NbFoisLettreDansMot(LettreJaune, textBoxChoix.Text[i]) + " dans les lettres jaunes ");
+                        }
+                    }
+                    else
+                    {
+                        //Parcours le mot entier pour trouver d'autres correspondances//
+
+                        for (int t = 0; t < reponse.Length; t++)
+                        {
+                            //Si la lettre actuel se situe sur une autre case//
+
+                            if (textBoxChoix.Text[i] == reponse[t])
+                            {
+                                //Passe la cellule en jaune//
+
+                                dataGridViewMotus.Rows[nbessai].Cells[i].Style.BackColor = Color.Yellow;
+
+                                //Pour arretter la boucle//
+
+                                t = reponse.Length;
+
+                                //Ajout de la lettre à la liste de lettre jaunes//
+
+                                LettreJaune += textBoxChoix.Text[i];
+                            }
+                        }
                     }
                 }
             }
-           
+
             nbessai++;
+
             if ((nbessai == nbessaimax) || (reponse == textBoxChoix.Text))
             {
-                // Affiche le message
+                //Affiche le message//
+
                 if (reponse == textBoxChoix.Text)
                 {
                     MessageBox.Show("Vous avez gagné ! Le mot est bien : " + reponse + ".", "Motus", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
@@ -242,10 +333,8 @@ namespace WindowsFormsApp1
                     MessageBox.Show("Vous avez perdu... Le mot est : " + reponse + ".", "Motus", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
 
-                // Cache le tableau
                 dataGridViewMotus.Visible = false;
 
-                // Bloque les controls
                 buttonValider.Enabled = false;
                 textBoxChoix.Enabled = false;
                 buttonAnnuler.Enabled = false;
@@ -254,12 +343,20 @@ namespace WindowsFormsApp1
                 textBoxFile.Enabled = true;
                 buttonTailleMotOuvrir.Enabled = true;
                 buttonJouer.Enabled = true;
-               
-            }
-           
+            } 
             textBoxChoix.Text = "";
-        }
-       
+        }       
+
         //==============================================================================
+
+        public int NbFoisLettreDansMot(string chaine, char lettre)
+        {
+            int nb = 0;
+            foreach (char c in chaine)
+            {
+                if (c == lettre) nb++;
+            }
+            return nb;  
+        }
     }
 }
